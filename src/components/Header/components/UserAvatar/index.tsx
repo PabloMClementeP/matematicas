9d4357fca44dc36c96c87eeeb46abfb51726cf78
Pablo Clemente <pclementep@gmail.com>
2024-10-09 14:44:11 -0300

    fix types

--- src/components/Header/components/UserAvatar/index.tsx
+++ src/components/Header/components/UserAvatar/index.tsx
@@ -2,20 +2,20 @@ import { useUserContext } from "../../../../context/UserContext";
 
 const USerAvatar = () => {
   const {
-    user: { avatar, name },
+    user,
   } = useUserContext();
   return (
     <div style={{display:'flex', flexDirection:'column', alignItems:'center', position:'relative', userSelect: 'none'}}>
       <img
-        src={avatar}
-        alt={`Avatar - ${name}`}
+        src={user?.avatar || ''}
+        alt={`Avatar - ${user?.name || ''} `}
         style={{
           width: "48px",
           height: "48px",
           borderRadius: "50%",
         }}
       />
-      <span style={{margin:0, padding:0, fontWeight:700, fontSize:'20px', position:'absolute', color:'white', bottom:0}}>{name}</span>
+      <span style={{margin:0, padding:0, fontWeight:700, fontSize:'20px', position:'absolute', color:'white', bottom:0}}>{user?.name}</span>
     </div>
   );
 };
